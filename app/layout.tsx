aff8596831dd08fcbc5a8b2112229361de16e6e1
dot-cloud-6005 <admin@dotsharedsite.com>
2025-08-15 08:37:02 +0800

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,15 @@ export const metadata: Metadata = {
   title: "Dev 6005",
   description:
     "Developed by 6005",
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', type: 'image/x-icon' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
